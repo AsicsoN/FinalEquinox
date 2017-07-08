d955ba3c0014972eb3721422f4d8c16f89dd484f
Jonathan Phelps <jphelps92@gmail.com>
2017-07-08 11:42:41 -0500

    add RamaSaveSystem plugin
    Former-commit-id: 3455afb0a771f0ee0f3a432d98eb2c308fa69e70

---
+++ Plugins/RamaSaveSystem/Intermediate/Build/Win64/UE4Editor/Inc/RamaSaveSystem/RamaSaveObject.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RAMASAVESYSTEM_RamaSaveObject_generated_h
+#error "RamaSaveObject.generated.h already included, missing '#pragma once' in RamaSaveObject.h"
+#endif
+#define RAMASAVESYSTEM_RamaSaveObject_generated_h
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_RPC_WRAPPERS
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_RPC_WRAPPERS_NO_PURE_DECLS
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesURamaSaveObject(); \
+	friend RAMASAVESYSTEM_API class UClass* Z_Construct_UClass_URamaSaveObject(); \
+public: \
+	DECLARE_CLASS(URamaSaveObject, UObject, COMPILED_IN_FLAGS(CLASS_Abstract), 0, TEXT("/Script/RamaSaveSystem"), NO_API) \
+	DECLARE_SERIALIZER(URamaSaveObject) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_INCLASS \
+private: \
+	static void StaticRegisterNativesURamaSaveObject(); \
+	friend RAMASAVESYSTEM_API class UClass* Z_Construct_UClass_URamaSaveObject(); \
+public: \
+	DECLARE_CLASS(URamaSaveObject, UObject, COMPILED_IN_FLAGS(CLASS_Abstract), 0, TEXT("/Script/RamaSaveSystem"), NO_API) \
+	DECLARE_SERIALIZER(URamaSaveObject) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API URamaSaveObject(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(URamaSaveObject) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, URamaSaveObject); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(URamaSaveObject); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API URamaSaveObject(URamaSaveObject&&); \
+	NO_API URamaSaveObject(const URamaSaveObject&); \
+public:
+
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API URamaSaveObject(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API URamaSaveObject(URamaSaveObject&&); \
+	NO_API URamaSaveObject(const URamaSaveObject&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, URamaSaveObject); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(URamaSaveObject); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(URamaSaveObject)
+
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_PRIVATE_PROPERTY_OFFSET
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_6_PROLOG
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_RPC_WRAPPERS \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_INCLASS \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_PRIVATE_PROPERTY_OFFSET \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_RPC_WRAPPERS_NO_PURE_DECLS \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_INCLASS_NO_PURE_DECLS \
+	HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h_10_ENHANCED_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_RamaSaveSystem_Source_RamaSaveSystem_Public_RamaSaveObject_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
