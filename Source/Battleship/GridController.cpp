a9caffcc3afe2f6d9c74ea31a073e814bc2fa1cd
Josh Hadley <josh.hadley@btinternet.com>
2017-10-17 18:59:28 +0100

    SM01 Added TileBP
    added TileBP to represent 'tiles' in grid.
    
    
    Former-commit-id: a083b57c4242d8ed5e89ee24bdbb9524d2b42218

--- Source/Battleship/GridController.cpp
+++ Source/Battleship/GridController.cpp
@@ -5,7 +5,7 @@
 AGridController::AGridController()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
