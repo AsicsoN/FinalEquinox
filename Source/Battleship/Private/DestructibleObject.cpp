4058046511a4cca4a27edadb6dfcc2850a1b3d57
Josh Hadley <josh.hadley@btinternet.com>
2017-10-31 20:14:10 +0000

    SM15 Added DestructibleObjects
    Former-commit-id: 4fcf68ccef3ed3da412af13370a13617df12ace3

---
+++ Source/Battleship/Private/DestructibleObject.cpp
@@ -0,0 +1,34 @@
+
+
+#include "Battleship.h"
+#include "DestructibleObject.h"
+
+
+// Sets default values
+ADestructibleObject::ADestructibleObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("Box"));
+	Box->SetBoxExtent(FVector(120.0f, 120.0f, 200.0f));
+	Box->SetupAttachment(Mesh);
+}
+
+// Called when the game starts or when spawned
+void ADestructibleObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADestructibleObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
