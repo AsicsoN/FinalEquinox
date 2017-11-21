d390e54d5f47fde799e09756c9695178736f641a
Josh Hadley <josh.hadley@btinternet.com>
2017-11-21 23:39:40 +0000

    Removed Build Discrepancies from 4.15
    Former-commit-id: 8f28be2777a98503b1dc17ab3218ca4540a27e02

--- Source/Battleship.Target.cs
+++ Source/Battleship.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class BattleshipTarget : TargetRules
 {
-	public BattleshipTarget(TargetInfo Target)
-	{
+	public BattleshipTarget(TargetInfo Target) : base(Target)
+    {
 		Type = TargetType.Game;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "Battleship" } );
-	}
+        ExtraModuleNames.Add("Battleship");
+    }
 }
