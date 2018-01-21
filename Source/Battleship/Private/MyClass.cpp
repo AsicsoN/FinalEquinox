82165e43d9d6e49b6d36d6d7f8b2c14ca29e0a9d
kevin <kevin.sawall@yahoo.com>
2018-01-21 14:43:31 -0500

    New VFX pack. Engine Trails hooked up. New Shield animation.
    Former-commit-id: 51a2755fc92bc5b8bfa2fad1bcda266abf7d0904

---
+++ Source/Battleship/Private/MyClass.cpp
@@ -0,0 +1,12 @@
+
+
+#include "Battleship.h"
+#include "MyClass.h"
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
---
+++ Source/Battleship/Public/MyClass.h
@@ -0,0 +1,15 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BATTLESHIP_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
