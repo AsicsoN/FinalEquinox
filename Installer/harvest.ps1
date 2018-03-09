e0a0b402f3f9833a227a62436fec47f064305ab4
jphelps92 <jphelps92@gmail.com>
2018-03-08 21:50:06 -0600

    update installer

--- Installer/harvest.ps1
+++ Installer/harvest.ps1
@@ -4,11 +4,11 @@ Write-Host "DirectoryToHarvest: $DirectoryToHarvest"
 Write-Host "OutFile: $OutFile"
 
 $heat = "$($env:WIX)bin\heat.exe"
-$parameters = "dir", "`"$DirectoryToHarvest`"", "-ag", "-cg", "UnrealFiles", "-dr", "INSTALLDIR", "-scom", "-sfrag", "-srd", "-sreg",
+$parameters = "dir", "`"$DirectoryToHarvest`"", "-ag", "-cg", "UnrealFiles", "-dr", "INSTALLDIR", "-scom", "-sfrag", "-srd", "-sreg", "-suid",
 "-var", "REPLACETHIS", "-out", "`"$OutFile`""
 $proc = Start-Process -FilePath $heat -ArgumentList $parameters -PassThru -Wait -NoNewWindow
 if ($proc.ExitCode -ne 0) {
 	exit $proc.ExitCode
 }
 
-(Get-Content $OutFile).replace('$(REPLACETHIS)', "$DirectoryToHarvest") | Set-Content $OutFile
+(Get-Content $OutFile).replace('$(REPLACETHIS)', "$DirectoryToHarvest").replace("Id=`"x64`"", "Id=`"id_x64`"") | Set-Content $OutFile
