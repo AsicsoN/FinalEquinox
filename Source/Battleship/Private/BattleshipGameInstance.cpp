af9b266d28d7001c0a96f8a85016275c5163cafd
jphelps92 <jphelps92@gmail.com>
2018-09-22 15:53:42 -0500

    Add saving and loading

--- Source/Battleship/Private/BattleshipGameInstance.cpp
+++ Source/Battleship/Private/BattleshipGameInstance.cpp
@@ -1,8 +1,15 @@
 #include "Battleship.h"
 #include "BattleshipGameInstance.h"
+#include "CharacterSheetInitialization.h"
 #include "AbilityLibrary.h"
 
 UBattleshipGameInstance::UBattleshipGameInstance(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
+	Initialize();
+}
 
+void UBattleshipGameInstance::Initialize()
+{
+	CharacterId = FGuid::NewGuid().ToString();
+	CharacterSheet = UCharacterSheetInitialization::CreateStartingCharacterSheet();
 }
